3d756ff2319c16bebd8c83d129e5ce8d3859cea7
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-09-20 15:52:45 -0400

    Allocation of work with employees into a separate controller

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _Employees = new()
+        {
+            new Employee { Id = 1, FirstName = "Петр", LastName = "Петров", Patronymic = "Петрович", Age = 31 },
+            new Employee { Id = 2, FirstName = "Иван", LastName = "Иванов", Patronymic = "Иванович", Age = 32 },
+            new Employee { Id = 3, FirstName = "Сидор", LastName = "Сидоров", Patronymic = "Сидорович", Age = 36 }
+        };
+        public IActionResult Index()
+        {
+            return View(_Employees);
+        }
+    }
+
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,26 +9,12 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
-       {
-            new Employee{Id = 1, FirstName = "Петр", LastName = "Петров", Patronymic = "Петрович", Age = 31},
-            new Employee{Id = 1, FirstName = "Иван", LastName = "Иванов", Patronymic = "Иванович", Age = 32},
-            new Employee { Id = 1, FirstName = "Сидор", LastName = "Сидоров", Patronymic = "Сидорович", Age = 36}
-        };
+        
         public IActionResult Index()
         {
             //return Content("Hello from controller!");
             return View();
         }
 
-        public IActionResult SecondAction(string id)
-        {
-            return Content($"Second action with parametre {id}");
-        }
-        public IActionResult Employees()
-        {
-            //return Content("Hello from controller!");
-            return View(_Employees);
-        }
     }
 }
