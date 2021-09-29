87ab864d6d16941cb89e14b7e238114cf6cf6c7f
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-09-29 11:05:16 -0400

    Presentation model data annotations

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -11,17 +11,17 @@ namespace WebStore.ViewModels
     {
         [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
-        [Display(Name ="")]
+        [Display(Name = "Имя")]
         [Required(ErrorMessage = "Name required")]
-        [StringLength(200,MinimumLength = 2,ErrorMessage = "")]
-        [RegularExpression(@"([A-z])",ErrorMessage = "")]
+        //[StringLength(200,MinimumLength = 2,ErrorMessage = "")]
+        //[RegularExpression(@"([A-z])",ErrorMessage = "")]
         public string Name { get; set; }
-        [Display()]
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
-        [Display()]
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
-        [Display()]
-        [Range(18,80,ErrorMessage = "")]
+        [Display(Name = "Возраст")]
+        //[Range(18,80,ErrorMessage = "")]
         public int Age { get; set; }
     }
 }
