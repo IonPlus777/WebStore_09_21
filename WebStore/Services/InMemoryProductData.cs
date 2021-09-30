7f4779f22d361c7fb89c81d40e9c5d31cff8c7af
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-09-30 05:16:52 -0400

    product catalog controller

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -10,10 +11,25 @@ namespace WebStore.Services
 {
     public class InMemoryProductData : IProductData
     {
+        public IEnumerable<Section> GetSections() => TestData.Sections;
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
 
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IEnumerable<Product> query = TestData.Products;
 
-        public IEnumerable<Section> GetSections() => TestData.Sections;
+            //if (Filter?.SectionId != null)
+            //    query = query.Where(p => p.SectionId == Filter.SectionId);
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(p => p.SectionId == section_id);
+
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(p => p.BrandId == brand_id);
+
+            return query;
+        }
+
+        
         
     }
 }
