8427b5f3323dd5a00054778ad731428cf455e423
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-09-30 03:38:33 -0400

    the logic of the visual component of brands

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     //[ViewComponent(Name = "BrandsView")]
     public class BrandsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
-
         //public async Task<IViewComponentResult> InvokeAsync() => View();
+        private readonly IProductData _ProductData;
+
+        public BrandsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() =>
+            _ProductData.GetBrands()
+            .OrderBy(b => b.Order)
+            .Select(b => new BrandViewModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+            });
+
+        
     }
 }
