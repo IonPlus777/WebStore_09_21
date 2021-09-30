1947e0a11d12cc590db9bec69755413bcff417d1
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-09-30 02:08:46 -0400

    blanks of decorative components

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsView")]
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+
+        //public async Task<IViewComponentResult> InvokeAsync() => View();
+    }
+}
