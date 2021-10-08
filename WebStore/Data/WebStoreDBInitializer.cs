372a3b10c7b6f5c8dddc91a96a8d33acbdf1d5bf
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-10-08 10:07:55 -0400

    Adding DB initialization logic with system data Identity

--- WebStore/Data/WebStoreDBInitializer.cs
+++ WebStore/Data/WebStoreDBInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -6,17 +7,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+        private readonly RoleManager<Role> _RoleManager;
         private readonly ILogger<WebStoreDBInitializer> _Logger;
-        public WebStoreDBInitializer(WebStoreDB db, ILogger<WebStoreDBInitializer> Logger)
+        
+        public WebStoreDBInitializer(
+            WebStoreDB db,
+            UserManager<User> UserManager,
+            RoleManager<Role> RoleManager,
+            ILogger<WebStoreDBInitializer> Logger)
         {
             _db = db;
+            _UserManager = UserManager;
+            _RoleManager = RoleManager;
             _Logger = Logger;
+
 
         }
 
@@ -43,7 +55,26 @@ namespace WebStore.Data
                 await _db.Database.MigrateAsync();
             }
 
-            await InitializeProductsAsync();
+            try 
+            { 
+                await InitializeProductsAsync();
+            }
+            catch(Exception e)
+            {
+                //Console.WriteLine(e);
+                _Logger.LogError(e, "Error of initialization of catalog of products");
+                throw;
+            }
+
+            try
+            {
+                await InitializeIdentityAsync();
+            }
+            catch(Exception e)
+            {
+                _Logger.LogError(e, "Error initialization of system Identity");
+                throw;
+            }
         }
 
         private async Task InitializeProductsAsync()
@@ -95,6 +126,57 @@ namespace WebStore.Data
                 await _db.Database.CommitTransactionAsync();
             }
             _Logger.LogInformation("Zapis tovarov vipolnena uspeshno za {0} ms",timer.Elapsed.TotalMilliseconds);
+        }
+
+        private async Task InitializeIdentityAsync()
+        {
+            _Logger.LogInformation("Initialization of system Identity");
+            var timer = Stopwatch.StartNew();
+
+            //if (!await _RoleManager.RoleExistsAsync(Role.Administrators))
+            //    await _RoleManager.CreateAsync(new Role { Name = Role.Administrators });
+
+            async Task CheckRole(string RoleName)
+            { 
+                if(await _RoleManager.RoleExistsAsync(RoleName))
+                _Logger.LogInformation("Role exist{0}",RoleName);
+                else
+                {
+                    _Logger.LogInformation("Role {0} don't exist",RoleName);
+                    await _RoleManager.CreateAsync(new Role { Name = RoleName });
+                    _Logger.LogInformation("Role {0} is created",RoleName);
+                }
+            }
+            await CheckRole(Role.Administrators);
+            await CheckRole(Role.Users);
+
+            if(await _RoleManager.FindByNameAsync(User.Administrator)is null)
+            {
+                _Logger.LogInformation("User {0} don't exist",User.Administrator);
+                var admin = new User
+                {
+                    UserName = User.Administrator,
+                };
+                var creation_result = await _UserManager.CreateAsync(admin, User.DefaultAdminPassword);
+                if(creation_result.Succeeded)
+                {
+                    _Logger.LogInformation("User {0} is created", User.Administrator);
+                    await _UserManager.AddToRoleAsync(admin, Role.Administrators);
+
+                    _Logger.LogInformation("Role of User {0} is created {1}", User.Administrator, Role.Administrators);
+                }
+                else 
+                {
+                    var errors = creation_result.Errors.Select(err => err.Description).ToArray();
+                    _Logger.LogError("Row of administrator don't created!Errors:{0}",string.Join(",",errors));
+
+                    throw new InvalidOperationException($"impossible create administrator{string.Join(",",errors)}");
+                }
+
+                _Logger.LogInformation("The donnes ofsystem Identity are created {0} mc", timer.Elapsed.TotalMilliseconds);
+
+            }
+
         }
        
     }
