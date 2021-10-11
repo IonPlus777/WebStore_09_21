e21236ae05bbc2581063735ce87e06d0806d5dd6
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-10-10 20:12:35 -0400

    Formation of the view model for the cart service

--- WebStore/Services/InSQL/SqlProductData.cs
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -33,11 +33,19 @@ namespace WebStore.Services.InSQL
 
             //if (Filter?.SectionId != null)
             //    query = query.Where(p => p.SectionId == Filter.SectionId);
+
+            if(Filter?.Ids.Length > 0)
+            {
+                query = query.Where(product => Filter.Ids.Contains(product.Id));
+            }
+            else 
+            { 
             if (Filter?.SectionId is { } section_id)
                 query = query.Where(p => p.SectionId == section_id);
 
             if (Filter?.BrandId is { } brand_id)
                 query = query.Where(p => p.BrandId == brand_id);
+            }
 
             return query;
         }
