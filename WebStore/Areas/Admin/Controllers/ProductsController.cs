1bd9a4adf51ff04b35ee00e07cb08608f8b41a59
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-10-19 13:50:17 -0400

    List of items in the catalog controller

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -3,15 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly IProductData _ProductData;
+
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+
         public IActionResult Index()
         {
-            return View();
+            var products = _ProductData.GetProducts();
+            return View(products);
         }
     }
 }
