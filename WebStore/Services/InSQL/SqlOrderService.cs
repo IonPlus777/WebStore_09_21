be082956a008ec603c3f19f126a12dcdd3a8d1ab
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-10-22 16:45:32 -0400

    Procurement of order service implementation

---
+++ WebStore/Services/InSQL/SqlOrderService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.InSQL
+{
+    public class SqlOrderService : IOrderService
+    {
+        private readonly WebStoreDB _db;
+        private readonly UserManager<User> _UserManager;
+
+        public SqlOrderService(WebStoreDB db,UserManager<User> UserManager)
+        {
+            _db = db;
+            _UserManager = UserManager;
+        }
+
+        public Task<Order> CreateOrder(string UserName, CartViewModel Cart, OrderViewModel OrderModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetUserOrders(string User)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
