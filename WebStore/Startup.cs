20a3b6b5d9d93d15c3eefe164b76720b4ae7f98a
NatalieDobrov <nataliia.dobrovolska@quasis.ca>
2021-10-22 18:42:43 -0400

    Checkout method in cart controller

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -82,6 +82,7 @@ namespace WebStore
 
             services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<ICartService, InCookiesCartService>();
+            services.AddScoped<IOrderService, SqlOrderService>();
 
 
             //services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
